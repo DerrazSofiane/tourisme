a1ac405f7f7accbb7843b7c98f475dd2386e4b6e
lee-roy <baltarzeasy@gmail.com>
2021-07-19 15:02:05 +0200

    rajout fct ariation par pays

--- interface.py
+++ interface.py
@@ -64,7 +64,7 @@ if mode == "Générique":
             """
             st.title("Volumes brutes des 2 dernières semaines")
             volume_brute = (volumes_brutes.set_index(list(fichier.columns)[0]))
-            st.write(volume_brute)
+            st.write(volume_brute.style.format({"{:.2f}"}))
             tableau = volumes_brutes
             # renommage de la 1ere colonne en "semaine"
             tableau = tableau.rename({list(fichier.columns)[0]: "semaine"}, 
