c13732de1efb80e58322be2beb642644b5e1a21c
lee-roy <baltarzeasy@gmail.com>
2021-07-21 09:23:22 +0200

    modif

--- interface.py
+++ interface.py
@@ -55,14 +55,14 @@ if mode == "Générique":
         top3_generique = generique_potentiel(variation, volumes_brutes)
 
     # CALCUL GENERIQUE
-        if st.sidebar.checkbox("Les tops") and uploaded_file != "None":
+        if st.sidebar.checkbox("1 - Les Tops") and uploaded_file != "None":
             # Checkbox de la partie "Les tops pays"
-            st.title("1- Les Tops")
+            st.title("1- Les meilleures pays pour le tourisme durant les 4 dernières semaines")
             colonnes = list(top3_generique.columns)
             st.write(top3_generique[colonnes[0]])
             st.write(top3_generique[colonnes[1]])
             st.write(top3_generique[colonnes[2]])
-        if st.sidebar.checkbox("Volumes brutes") and uploaded_file != "None":
+        if st.sidebar.checkbox("Volumes bruts") and uploaded_file != "None":
             # Checkbox de la partie Volume brutes des 2 dernières semaines
             st.title("Volumes brutes des 2 dernières semaines")
             volume_brute = volumes_brutes.set_index(list(fichier.columns)[0])
