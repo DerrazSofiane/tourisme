2631752ce209464090a2bcdef4da55f4e0666bdb
lmannier <66322890+Lee-RoyMannier@users.noreply.github.com>
2021-08-18 16:52:51 +0200

    Drive

--- interface.py
+++ interface.py
@@ -38,7 +38,7 @@ def lecture_donnees(nom_tableau, DATA_DRIVE):
     # est lu, transformé en tableau, reformaté et renvoyé
     id_drive = DATA_DRIVE[nom_tableau]
     gauth = GoogleAuth()
-    gauth.LoadCredentialsFile("mycreds.txt")
+    gauth.LoadCredentialsFile("C:/mycreds.txt")
     drive = GoogleDrive(gauth)
     fichier_source = drive.CreateFile({'id': id_drive})
     donnee_brut = fichier_source.GetContentString()
@@ -668,7 +668,7 @@ def connexion_drive(id_dossier):
     
     gauth = GoogleAuth()
     
-    gauth.LoadCredentialsFile("mycreds.txt")
+    gauth.LoadCredentialsFile("C:/mycreds.txt")
     # if gauth.credentials is None:
     #     # Authenticate if they're not there
     #     gauth.LocalWebserverAuth()
