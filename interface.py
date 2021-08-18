813eec3a09ffd7e3c61a47931651d12ce09d6fc9
lmannier <66322890+Lee-RoyMannier@users.noreply.github.com>
2021-08-18 11:44:11 +0200

    Drive

--- interface.py
+++ interface.py
@@ -38,7 +38,7 @@ def lecture_donnees(nom_tableau, DATA_DRIVE):
     # est lu, transformé en tableau, reformaté et renvoyé
     id_drive = DATA_DRIVE[nom_tableau]
     gauth = GoogleAuth()
-    gauth.CommandLineAuth()
+    gauth.GetAuthUrl()
     #gauth.LoadCredentialsFile("mycreds.txt")
     drive = GoogleDrive(gauth)
     fichier_source = drive.CreateFile({'id': id_drive})
@@ -666,8 +666,8 @@ def connexion_drive(id_dossier):
     # doit se trouver dans le dossier racine du projet.
     # /app/tourisme : dossier relatif en ligne
     gauth = GoogleAuth()
-    gauth.CommandLineAuth()
-    gauth.LoadCredentialsFile("mycreds.txt")
+    gauth.GetAuthUrl()
+    #gauth.LoadCredentialsFile("mycreds.txt")
     # if gauth.credentials is None:
     #     # Authenticate if they're not there
     #     gauth.LocalWebserverAuth()
