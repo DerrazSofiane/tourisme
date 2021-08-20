172983cf568f46f5b58ad21611ae9a091d82dc35
lmannier <66322890+Lee-RoyMannier@users.noreply.github.com>
2021-08-20 17:01:11 +0200

    dépot data

--- interface.py
+++ interface.py
@@ -673,6 +673,7 @@ def interface(CONTENU_GLOBAL):
                                   engine='python')
             decompose = donnee_tourisme.split("_")
             type_analyse = decompose[1]
+            type_analyse[:8]
             if type_analyse[:8] != "Generique":
                 type_analyse = decompose[0] + ": " + decompose[1]
             # type_analyse = analyse.columns[titre_index]
